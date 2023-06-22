70ca652b0338461af6e4ac3e7738cc349ef0cd50
geniuszmath75 <geniuszmath75@gmail.com>
2023-06-22 17:40:06 +0200

    Home page and information page.

---
+++ src/components/pages/CountryInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import InfoPage from "../InfoPage";
+
+function CountryInfo() {
+  return (
+      <InfoPage />
+  );
+}
+
+export default CountryInfo;
---
+++ src/components/pages/SearchCountry.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Countries from '../Countries'
+
+function SearchCountry() {
+  return (
+    <Countries />
+  )
+}
+
+export default SearchCountry
