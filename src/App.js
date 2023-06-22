f1615751bda8e3cefa44d58b415dcd2177e62423
geniuszmath75 <geniuszmath75@gmail.com>
2023-06-22 17:37:39 +0200

    Creating app wireframe.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SearchCountry from "./components/pages/SearchCountry";
+import CountryInfo from "./components/pages/CountryInfo";
+import axios from "axios";
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/independent?status=true")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const names = data.map((country) => 
+    country.name.common.includes(" ") === true ? country.name.common.replace(/\s/g, "_") : country.name.common);
+
+  const keys = data.map((country) => country.population);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" Component={SearchCountry} />
+          {names.map((country, key) => {
+            return (
+              <Route key={keys[key]} path={`/${country}`} Component={CountryInfo} />
+            );
+          })}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
