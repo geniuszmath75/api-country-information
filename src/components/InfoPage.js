9f23696ccdc276ead4bcfded05ad01e0b8ebe7df
geniuszmath75 <geniuszmath75@gmail.com>
2023-06-25 17:53:11 +0200

    Creating a page that displays detailed information about given country.

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -1,25 +1,35 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Info from "./Info";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
+import "../ShortNames.js";
+import ShortNames from "../ShortNames.js";
 
 function InfoPage() {
   const [countryInfo, setCountryInfo] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+
   const location = useLocation();
   const name =
     location.pathname.includes("_") === true
-      ? location.pathname.replace("_", " ").substring(1)
+      ? location.pathname.replace(/_/g, " ").substring(1)
       : location.pathname.substring(1);
 
+  const clickedLink = () => {
+    setRefresh(true);
+  };
+
   useEffect(() => {
+    if (refresh) window.location.reload();
+
     axios
-      .get(`https://restcountries.com/v3.1/name/${name}`)
+      .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
       .then((res) => {
         const data = Object.assign({}, ...res.data);
         setCountryInfo(data);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [refresh]);
 
   return (
     <div className="info-container">
@@ -29,14 +39,15 @@ function InfoPage() {
         countryInfo.languages &&
         countryInfo.idd &&
         countryInfo.currencies &&
-        countryInfo.timezones && (
+        countryInfo.timezones &&
+        ShortNames && (
           <Info
-            key={countryInfo.population}
+            key={countryInfo.cca3}
             flags={countryInfo.flags.png}
             common={countryInfo.name.common}
             nativename1={Object.values(countryInfo.name.nativeName)[0].official}
             nativename2={Object.values(countryInfo.name.nativeName)[0].common}
-            altspel={countryInfo.altSpellings?.map((alt, index) => {
+            altspel={countryInfo.altSpellings.map((alt, index) => {
               return index === countryInfo.altSpellings.length - 1
                 ? alt
                 : alt + ", ";
@@ -54,24 +65,59 @@ function InfoPage() {
             subregion={countryInfo.subregion}
             capital={countryInfo.capital}
             area={countryInfo.area}
-            borders={countryInfo.borders?.map((border, index) => {
-              return index === countryInfo.borders.length - 1
-                ? border
-                : border + ", ";
-            })}
+            borders={
+              countryInfo["borders"] === undefined
+                ? "None (island)"
+                : countryInfo.borders.map((border, index) => {
+                    for (let i = 0; i < ShortNames.length; i++) {
+                      for (const key in ShortNames[i]) {
+                        if (
+                          ShortNames[i].hasOwnProperty(key) &&
+                          ShortNames[i][key] === border.toLowerCase()
+                        ) {
+                          return index === countryInfo.borders.length - 1 ? (
+                            <Link
+                              key={index}
+                              className="border-links"
+                              to={"/" + ShortNames[i].name}
+                              onClick={clickedLink}
+                            >
+                              {border}
+                            </Link>
+                          ) : (
+                            <Link
+                              key={index}
+                              className="border-links"
+                              to={"/" + ShortNames[i].name}
+                              onClick={clickedLink}
+                            >
+                              {border + ", "}
+                            </Link>
+                          );
+                        }
+                      }
+                    }
+                  })
+            }
             landlocked={countryInfo.landlocked === true ? "Yes" : "No"}
             isoa2={countryInfo.cca2}
             isoa3={countryInfo.cca3}
             ison={countryInfo.ccn3}
-            icc={countryInfo.idd.root + countryInfo.idd.suffixes}
+            icc={
+              countryInfo.idd.suffixes.length > 1
+                ? countryInfo.idd.root
+                : countryInfo.idd.root + countryInfo.idd.suffixes
+            }
             isocc={Object.keys(countryInfo.currencies)[0]}
             population={countryInfo.population}
             timezone={countryInfo.timezones[0]}
             currency={
-              Object.values(countryInfo.currencies)[0].name +
-              "(" +
-              Object.values(countryInfo.currencies)[0].symbol +
-              ")"
+              Object.values(countryInfo.currencies)[0].symbol !== undefined
+                ? Object.values(countryInfo.currencies)[0].name +
+                  "(" +
+                  Object.values(countryInfo.currencies)[0].symbol +
+                  ")"
+                : Object.values(countryInfo.currencies)[0].name
             }
             tldomain={countryInfo.tld[0]}
           />
